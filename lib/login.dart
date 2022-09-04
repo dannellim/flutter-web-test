2aef3e43ce189f5fbaff6a6e4099fd1452730a2b
Dannel Lim Khye Hsen <dannel.limkhyehsen@azqore.com>
2022-09-04 20:37:06 +0800

    add login page

---
+++ lib/login.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/UI/input_field.dart';
+import 'package:flutter_application/home.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //Scaffold
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Theme.of(context).backgroundColor,
+        child: Stack(
+          children: <Widget>[
+            const Align(
+              alignment: Alignment.bottomRight,
+              heightFactor: 0.5,
+              widthFactor: 0.5,
+              child: Material(
+                borderRadius: BorderRadius.all(Radius.circular(200.0)),
+                color: Color.fromRGBO(255, 255, 255, 0.4),
+                child: SizedBox(
+                  width: 400,
+                  height: 400,
+                ),
+              ),
+            ),
+            Center(
+              child: SizedBox(
+                width: 400,
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Material(
+                        elevation: 10.0,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(80.0)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Image.asset(
+                            "images/yokogawa.png",
+                            width: 60,
+                            height: 60,
+                          ),
+                        )),
+                    const Form(
+                      child: InputField(
+                          //Calling inputField  class
+                          Icon(
+                            Icons.person,
+                            color: Colors.white,
+                          ),
+                          "Username"),
+                    ),
+                    const Form(
+                      child: InputField(
+                          Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                          "Password"),
+                    ),
+                    SizedBox(
+                      width: 150,
+                      height: 80,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const MyHomePage(
+                                    title: 'Yokogawa',
+                                  )));
+                        },
+                        child: const Text(
+                          "Login",
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
